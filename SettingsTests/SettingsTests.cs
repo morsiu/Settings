07e6ee5d62e30a1f07b9c4d8f4cf810ded3776dc
Łukasz Mrozek <morskk@gmail.com>
2013-03-23 00:19:24 +0100

    Added SettingsTests for testing Settings static class members behavior.

---
+++ SettingsTests/SettingsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SettingsTests
+{
+    [TestClass]
+    public class SettingsTests
+    {
+        [TestMethod]
+        public void ShouldGetDistinctEmptyCollectionsAsSettingsPropertyDefaultValueForDifferentObjects()
+        {
+            var objectA = new DependencyObject();
+            var objectB = new DependencyObject();
+            var collectionA = Settings.Binding.Wpf.Markup.Settings.GetSettings(objectA);
+            var collectionB = Settings.Binding.Wpf.Markup.Settings.GetSettings(objectB);
+            Assert.AreNotSame(collectionA, collectionB);
+        }
+    }
+}
