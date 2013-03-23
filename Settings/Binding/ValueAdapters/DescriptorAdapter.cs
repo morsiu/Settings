0c44cb4952f0c4d48b3d1ddca1f7aeae52c0deca
Łukasz Mrozek <morskk@gmail.com>
2013-03-23 23:57:35 +0100

    Implemented IDisposable for DescriptorAdapter.

--- Settings/Binding/ValueAdapters/DescriptorAdapter.cs
+++ Settings/Binding/ValueAdapters/DescriptorAdapter.cs
@@ -3,42 +3,62 @@ using System.ComponentModel;
 
 namespace Settings.Binding.ValueAdapters
 {
-    public class DescriptorAdapter : IValueAdapter
+    public class DescriptorAdapter : IValueAdapter, IDisposable
     {
         private readonly PropertyDescriptor _descriptor;
         private readonly object _target;
+        private bool _isDisposed;
         private Action<object> _valueChangedCallback;
 
         public DescriptorAdapter(object target, PropertyDescriptor descriptor)
         {
             _target = target;
             _descriptor = descriptor;
+            _valueChangedCallback = newValue => { };
             descriptor.AddValueChanged(target, OnValueChanged);
         }
 
         private void OnValueChanged(object sender, EventArgs e)
         {
-            if (_valueChangedCallback != null)
-            {
-                var value = GetValue();
-                _valueChangedCallback(value);
-            }
+            var value = GetValue();
+            _valueChangedCallback(value);
         }
 
         public Action<object> ValueChangedCallback
         {
-            set { _valueChangedCallback = value; }
+            set 
+            {
+                FailIfDisposed();
+                _valueChangedCallback = value ?? (newValue => { });
+            }
         }
 
         public object GetValue()
         {
+            FailIfDisposed();
             var value = _descriptor.GetValue(_target);
             return value;
         }
 
         public void SetValue(object value)
         {
+            FailIfDisposed();
             _descriptor.SetValue(_target, value);
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+            _descriptor.RemoveValueChanged(_target, OnValueChanged);
+            _isDisposed = true;
+        }
+
+        private void FailIfDisposed()
+        {
+            if (_isDisposed) throw new ObjectDisposedException("DescriptorAdapter");
         }
     }
 }
