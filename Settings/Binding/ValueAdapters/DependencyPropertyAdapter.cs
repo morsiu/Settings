8df52b02616522eb5646484e0b80134902f8493a
Łukasz Mrozek <morskk@gmail.com>
2013-03-23 16:57:18 +0100

    Implemented IDisposable and added support for readonly properties for DependencyPropertyAdapter.

--- Settings/Binding/ValueAdapters/DependencyPropertyAdapter.cs
+++ Settings/Binding/ValueAdapters/DependencyPropertyAdapter.cs
@@ -4,15 +4,18 @@ using System.Windows;
 
 namespace Settings.Binding.ValueAdapters
 {
-    public class DependencyPropertyAdapter : IValueAdapter
+    public sealed class DependencyPropertyAdapter : IValueAdapter, IDisposable
     {
         private readonly DependencyProperty _property;
         private readonly DependencyPropertyDescriptor _descriptor;
         private readonly DependencyObject _target;
+        private bool _isDisposed;
         private Action<object> _valueChangedCallback;
 
         public DependencyPropertyAdapter(DependencyObject target, DependencyProperty property)
         {
+            if (target == null) throw new ArgumentNullException("target");
+            if (property == null) throw new ArgumentNullException("property");
             _target = target;
             _property = property;
             _descriptor = DependencyPropertyDescriptor.FromProperty(property, property.OwnerType);
@@ -31,17 +34,41 @@ namespace Settings.Binding.ValueAdapters
 
         public Action<object> ValueChangedCallback
         {
-            set { _valueChangedCallback = value; }
+            set 
+            {
+                FailIfDisposed();
+                _valueChangedCallback = value;
+            }
         }
 
         public object GetValue()
         {
+            FailIfDisposed();
             return _target.GetValue(_property);
         }
 
         public void SetValue(object value)
         {
-            _target.SetValue(_property, value);
+            FailIfDisposed();
+            if (!_property.ReadOnly)
+            {
+                _target.SetValue(_property, value);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+            _descriptor.RemoveValueChanged(_target, PropertyChangedHandler);
+            _isDisposed = true;
+        }
+
+        private void FailIfDisposed()
+        {
+            if (_isDisposed) throw new ObjectDisposedException("DependencyPropertyAdapter");
         }
     }
 }
