62772635d10725ad9236295824dac8fc48effdbf
Łukasz Mrozek <morskk@gmail.com>
2013-04-09 11:36:16 +0200

    Removed unused usings.

--- Sample/View/UserControl1.xaml.cs
+++ Sample/View/UserControl1.xaml.cs
@@ -6,7 +6,6 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using TheSettings;
 using TheSettings.Binding.Accessors;
 using TheSettings.Stores;
 using TheSettings.Wpf;
