fcb8317b608d7519873dc3f408d35a5fd081380d
Łukasz Mrozek <morskk@gmail.com>
2013-04-20 14:59:45 +0200

    Added abtract base class implementing IDisposable.

---
+++ Settings/Infrastructure/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSettings.Infrastructure
+{
+    public abstract class Disposable : IDisposable
+    {
+        protected bool IsDiposed { get; private set; }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+        }
+
+        protected void CheckNotDisposed()
+        {
+            if (IsDiposed)
+            {
+                throw new ObjectDisposedException(null);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (IsDiposed)
+            {
+                return;
+            }
+            Dispose(true);
+            IsDiposed = true;
+        }
+    }
+}
