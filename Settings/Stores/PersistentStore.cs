cd8ae8b74afc6ed54e2b854327a2dec4181d63bb
Łukasz Mrozek <morskk@gmail.com>
2013-04-23 11:20:10 +0200

    Simplified PersistentStore.

--- Settings/Stores/PersistentStore.cs
+++ Settings/Stores/PersistentStore.cs
@@ -4,28 +4,21 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
-using System.Collections.Generic;
-using TheSettings.Infrastructure;
 
 namespace TheSettings.Stores
 {
     public class PersistentStore : ISettingsStore
     {
         private readonly ISettingsStore _store;
-        private readonly Action<ICollection<Setting>> _storeAction;
-        private readonly Func<ICollection<Setting>, bool> _storePolicy;
-        private HashSet<Setting> _settings = new HashSet<Setting>(new SettingNameNamespaceEqualityComparer());
+        private readonly Action<Setting> _storeAction;
 
         public PersistentStore(
             ISettingsStore store,
-            Func<ICollection<Setting>, bool> storePolicy,
-            Action<ICollection<Setting>> storeAction)
+            Action<Setting> storeAction)
         {
             if (store == null) throw new ArgumentNullException("store");
-            if (storePolicy == null) throw new ArgumentNullException("storePolicy");
             if (storeAction == null) throw new ArgumentNullException("storeAction");
             _store = store;
-            _storePolicy = storePolicy;
             _storeAction = storeAction;
         }
 
@@ -37,28 +30,12 @@ namespace TheSettings.Stores
         public void SetSetting(SettingsNamespace @namespace, string name, object value)
         {
             _store.SetSetting(@namespace, name, value);
-            AddSetting(new Setting(@namespace, name, value));
-            if (ShouldStoreSettings())
-            {
-                StoreSettings();
-            }
+            StoreSetting(new Setting(@namespace, name, value));
         }
 
-        private bool ShouldStoreSettings()
+        private void StoreSetting(Setting setting)
         {
-            return _storePolicy(_settings);
-        }
-
-        private void StoreSettings()
-        {
-            var settingsToStore = _settings;
-            _settings = new HashSet<Setting>(new SettingNameNamespaceEqualityComparer());
-            _storeAction(settingsToStore);
-        }
-
-        private void AddSetting(Setting setting)
-        {
-            _settings.Add(setting);
+            _storeAction(setting);
         }
     }
 }
--- SettingsTests/StoreTests/PersistentStoreTests.cs
+++ SettingsTests/StoreTests/PersistentStoreTests.cs
@@ -3,6 +3,7 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -16,69 +17,19 @@ namespace TheSettingsTests.StoreTests
     public class PersistentStoreTests : DelegatingStoreTestSuite
     {
         [TestMethod]
-        public void ShouldNotStoreSettingsIfPolicyProhibits()
+        public void ShouldPassSettingToStore()
         {
             var innerStore = new Mock<ISettingsStore>();
-            bool wasCalled = false;
-            var store = new PersistentStore(innerStore.Object, settings => false, settings => { wasCalled = true; });
-            store.SetSetting(null, null, null);
-            Assert.IsFalse(wasCalled);
-        }
-
-        [TestMethod]
-        public void ShouldPassSettingsToPolicy()
-        {
-            var innerStore = new Mock<ISettingsStore>();
-            var setting = new Setting(new SettingsNamespace("NS"), "Name", new object());
+            var expectedSetting = new Setting(new SettingsNamespace("NS"), "Name", new object());
             Setting actualSetting = null;
-            var store = new PersistentStore(
-                innerStore.Object,
-                settings =>
-                {
-                    actualSetting = settings.FirstOrDefault();
-                    return true;
-                },
-                settings => { });
-            store.SetSetting(setting.Namespace, setting.Name, setting.Value);
-            Assert.AreEqual(setting, actualSetting);
-        }
-
-        [TestMethod]
-        public void ShouldStoreSettingsIfPolicySaysSo()
-        {
-            var innerStore = new Mock<ISettingsStore>();
-            bool wasCalled = false;
-            var store = new PersistentStore(innerStore.Object, settings => true, settings => { wasCalled = true; });
-            store.SetSetting(null, null, null);
-            Assert.IsTrue(wasCalled);
-        }
-
-        [TestMethod]
-        public void ShouldPassSettingsForStore()
-        {
-            var innerStore = new Mock<ISettingsStore>();
-            var setting = new Setting(new SettingsNamespace("NS"), "Name", new object());
-            Setting actualSetting = null;
-            var store = new PersistentStore(innerStore.Object, settings => true, settings => { actualSetting = settings.FirstOrDefault(); });
-            store.SetSetting(setting.Namespace, setting.Name, setting.Value);
-            Assert.AreEqual(setting, actualSetting);
-        }
-
-        [TestMethod]
-        public void ShouldClearSettingsAfterStore()
-        {
-            var innerStore = new Mock<ISettingsStore>();
-            var setting = new Setting(new SettingsNamespace("NS"), "Name", new object());
-            var settingsList = new List<List<Setting>>();
-            var store = new PersistentStore(innerStore.Object, settings => true, settings => settingsList.Add(settings.ToList()));
-            store.SetSetting(setting.Namespace, setting.Name, setting.Value);
-            store.SetSetting(null, null, null);
-            CollectionAssert.AreNotEquivalent(settingsList[0], settingsList[1]);
+            var store = new PersistentStore(innerStore.Object, setting => { actualSetting = setting; });
+            store.SetSetting(expectedSetting.Namespace, expectedSetting.Name, expectedSetting.Value);
+            Assert.AreEqual(expectedSetting, actualSetting);
         }
 
         protected override ISettingsStore ProvideStore(ISettingsStore innerStore)
         {
-            return new PersistentStore(innerStore, settings => true, settings => { });
+            return new PersistentStore(innerStore, settings => { });
         }
     }
 }
