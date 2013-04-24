3033d597f06cebfd1ec770581c9ea1e8e6ccb348
Łukasz Mrozek <morskk@gmail.com>
2013-04-24 16:15:36 +0200

    Extracted interface from InvalidatableSettingsStore.

--- Settings/Stores/IInvalidatableSettingsStore.cs
+++ Settings/Stores/IInvalidatableSettingsStore.cs
@@ -3,40 +3,10 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using System;
-
 namespace TheSettings.Stores
 {
-    /// <summary>
-    /// A store that allows its contents to be invalidated upon request.
-    /// </summary>
-    public class InvalidatableSettingsStore : IInvalidatableSettingsStore
+    public interface IInvalidatableSettingsStore : ISettingsStore
     {
-        private readonly Func<ISettingsStore, ISettingsStore> _invalidator;
-        private ISettingsStore _store;
-
-        public InvalidatableSettingsStore(
-            ISettingsStore store,
-            Func<ISettingsStore, ISettingsStore> invalidator)
-        {
-            if (invalidator == null) throw new ArgumentNullException("invalidator");
-            _store = store;
-            _invalidator = invalidator;
-        }
-
-        public void Invalidate()
-        {
-            _store = _invalidator(_store);
-        }
-
-        public object GetSetting(SettingsNamespace @namespace, string name)
-        {
-            return _store.GetSetting(@namespace, name);
-        }
-
-        public void SetSetting(SettingsNamespace @namespace, string name, object value)
-        {
-            _store.SetSetting(@namespace, name, value);
-        }
+        void Invalidate();
     }
 }
