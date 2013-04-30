bb8be7ba4bb4bdf4b62eb1c4476a73145d005d38
Łukasz Mrozek <morskk@gmail.com>
2013-04-30 16:33:24 +0200

    DataGridColumsBinding returns bindings that use TypeConverter assigned to stored properties.

--- Settings.Wpf/Binding/DataGridColumnsBinding.cs
+++ Settings.Wpf/Binding/DataGridColumnsBinding.cs
@@ -3,11 +3,15 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using TheSettings.Binding;
+using TheSettings.Binding.ValueAdapters;
+using TheSettings.Wpf.Binding.ValueConverters;
 
 namespace TheSettings.Wpf.Binding
 {
@@ -53,11 +57,40 @@ namespace TheSettings.Wpf.Binding
             return
                 from storedProperty in StoredProperties
                 let name = string.Format(ColumnSettingFormat, index, storedProperty.Name)
+                let targetAdapter = CreateTargetAdapter(column, storedProperty)
                 let binding = builder
-                    .SetTargetAdapter(column, storedProperty)
+                    .SetTargetAdapter(targetAdapter)
                     .SetSourceAdapter(accessor, Store, @namespace, name)
                     .Build()
                 select binding;
+        }
+
+        private IValueAdapter CreateTargetAdapter(DataGridColumn column, DependencyProperty storedProperty)
+        {
+            var propertyType = storedProperty.PropertyType;
+            var propertyAdapter = new DependencyPropertyAdapter(column, storedProperty);
+            var typeConverter = GetTypeConverterIfAny(propertyType);
+            if (typeConverter != null)
+            {
+                var valueConverter = new TypeConverterAdapter(typeConverter, typeof(string));
+                return new ConvertingAdapter(propertyAdapter, valueConverter);
+            }
+            return propertyAdapter;
+        }
+
+        private static TypeConverter GetTypeConverterIfAny(Type propertyType)
+        {
+            if (!propertyType.IsDefined(typeof(TypeConverterAttribute), false))
+            {
+                return null;
+            }
+            var converterAttribute = propertyType.GetCustomAttributes(typeof(TypeConverterAttribute), false).OfType<TypeConverterAttribute>().First();
+            var converterType = Type.GetType(converterAttribute.ConverterTypeName, false);
+            if (converterType == null || !typeof(TypeConverter).IsAssignableFrom(converterType))
+            {
+                return null;
+            }
+            return (TypeConverter)Activator.CreateInstance(converterType);
         }
     }
 }
