e00fae63aeea6cae63e7044cec0b038bf14006ef
Łukasz Mrozek <morskk@gmail.com>
2013-06-11 21:24:06 +0200

    Added missing constructor parameter null check.

--- Settings/Binding/Accessors/SingleSettingsStoreAccessor.cs
+++ Settings/Binding/Accessors/SingleSettingsStoreAccessor.cs
@@ -3,6 +3,8 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
+
 namespace TheSettings.Binding.Accessors
 {
     public class SingleSettingsStoreAccessor : ISettingsStoreAccessor
@@ -11,6 +13,7 @@ namespace TheSettings.Binding.Accessors
 
         public SingleSettingsStoreAccessor(ISettingsStore store)
         {
+            if (_store == null) throw new ArgumentNullException("store");
             _store = store;
         }
 
