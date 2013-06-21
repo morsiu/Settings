502d3e3a6c9014d6bafba1c0b689a255c3419002
Łukasz Mrozek <morskk@gmail.com>
2013-06-21 14:11:35 +0200

    Trying to fix issue with ListUpdater inserting item past updatet list.

--- Settings/Infrastructure/Collections/ListUpdater.cs
+++ Settings/Infrastructure/Collections/ListUpdater.cs
@@ -76,6 +76,11 @@ namespace TheSettings.Infrastructure.Collections
 
         private void InsertItems(IEnumerable newItems, int startingIndex)
         {
+            if (startingIndex >= _targetList.Count)
+            {
+                AddItems(newItems);
+                return;
+            }
             foreach (var item in newItems.OfType<object>().Reverse())
             {
                 _targetList.Insert(startingIndex, item);
