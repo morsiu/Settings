378a9db9a98c8e622367ec2a358d7374a4f61cf0
Łukasz Mrozek <morskk@gmail.com>
2015-01-07 09:03:04 +0100

    Removed unused usings.

--- Settings/Binding/ValueAdapters/ValueToCollectionAdapter.cs
+++ Settings/Binding/ValueAdapters/ValueToCollectionAdapter.cs
@@ -5,11 +5,8 @@
 
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TheSettings.Infrastructure;
 
 namespace TheSettings.Binding.ValueAdapters
