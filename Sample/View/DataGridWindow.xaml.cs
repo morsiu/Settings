2e09a5d11d1a55dd9620166d223b8087dc4a9d16
Łukasz Mrozek <morskk@gmail.com>
2015-09-15 14:57:49 +0200

    Added sample window with data grid

--- Sample/View/DataGridWindow.xaml.cs
+++ Sample/View/DataGridWindow.xaml.cs
@@ -1,44 +1,17 @@
-﻿// Copyright (C) 2013 Łukasz Mrozek
+﻿// Copyright (C) 2015 Łukasz Mrozek
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using TheSettings.Binding.Accessors;
-using TheSettings.Stores;
-using TheSettings.Wpf;
 
 namespace Sample.View
 {
-    public partial class UserControl1 : UserControl
+    public partial class DataGridWindow : Window
     {
-        private readonly SettingsStore _store;
-
-        public UserControl1()
+        public DataGridWindow()
         {
             InitializeComponent();
-            var accessor = (SingleSettingsStoreAccessor)Settings.CurrentStoreAccessor;
-            _store = (SettingsStore)accessor.Store;
-            _store.SettingChanged += (o, e) => RefreshSettings();
-        }
-
-        private void RefreshSettingsSource(object sender, RoutedEventArgs e)
-        {
-            RefreshSettings();
-        }
-
-        private void RefreshSettings()
-        {
-            var allSettings = _store.SettingsByNamespace
-                .SelectMany(
-                    n => n.Value.Settings,
-                    (n, c) => new { Namespace = n.Key, Key = c.Key, Value = c.Value })
-                .OrderBy(n => n.Namespace.ToString())
-                .ThenBy(n => n.Key)
-                .ToList();
-            SettingsDataGrid.ItemsSource = allSettings;
         }
     }
 }
--- Sample/View/Test.xaml.cs
+++ Sample/View/Test.xaml.cs
@@ -3,6 +3,7 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Sample.View
@@ -12,6 +13,11 @@ namespace Sample.View
         public Test()
         {
             InitializeComponent();
+        }
+
+        private void OnDataGridWindowButtonClick(object sender, RoutedEventArgs e)
+        {
+            new DataGridWindow().ShowDialog();
         }
     }
 }
